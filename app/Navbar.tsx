30e9e1728c7a440d11a4e8d2697aa4123550c763
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-16 01:26:57 +0600

    Building the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
 
 export const Navbar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issue", href: "/issues" },
   ];
+
   return (
-    <div>
-      <Link href="/">Logo</Link>
-    </div>
+    <nav className="flex space-x-6 items-center border-b mb-5 px-6 h-14">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link key={link.href} href={link.href}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
   );
 };
