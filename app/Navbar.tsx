521d6ed45877a4fa32fbb2c3a51414384fa2675a
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-16 01:34:50 +0600

    Styling the active link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,16 @@
-import Link from "next/link";
-import { AiFillBug } from "react-icons/ai";
+"use client";
 
-export const Navbar = () => {
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+
+export const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issue", href: "/issues" },
+    { label: "Issues", href: "/issues" },
   ];
 
   return (
@@ -14,7 +20,15 @@ export const Navbar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link key={link.href} href={link.href}>
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
             {link.label}
           </Link>
         ))}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Navbar } from "./Navbar";
+import { NavBar } from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
